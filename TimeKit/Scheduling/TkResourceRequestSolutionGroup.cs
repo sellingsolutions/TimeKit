b8b08a57f03480a5257dc3704a8d3940c1c58956
Alexander Selling <alexander@icontrolapp.se>
2019-04-24 13:48:26 +0200

    Renamed the request group to solution group

---
+++ TimeKit/Scheduling/TkResourceRequestSolutionGroup.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeKit.DataStructure;
+using TimeKit.Models;
+
+namespace TimeKit.Scheduling
+{
+    public class TkResourceRequestSolutionGroup
+    {
+        public TkObjectType ObjectType { get; set; }
+        public List<long> WeekNumbers { get; set; }
+
+        public long NoOfObjects { get; set; }
+        public long MinutesRequiredPerObject { get; set; }
+
+        public TimeSpan TicksRequiredPerObject => TimeSpan.FromMinutes(MinutesRequiredPerObject);
+        public TimeSpan TotalTicksRequired => TimeSpan.FromMinutes(MinutesRequiredPerObject * NoOfObjects);
+
+        public List<TkActor> AvailableActors { get; set; }
+        public List<TkProcess> AvailableProcesses { get; set; }
+
+        public List<TkResourceRequestSolutionRow> Rows { get; set; } = new List<TkResourceRequestSolutionRow>();
+
+        public TkResourceRequestSolutionGroup(
+            TkObjectType objectType, 
+            long noOfObjects, 
+            long minutesRequiredPerObject, 
+            List<long> weekNumbers, 
+            List<TkActor> availableActors, 
+            List<TkProcess> availableProcesses)
+        {
+
+            ObjectType = objectType;
+            NoOfObjects = noOfObjects;
+            MinutesRequiredPerObject = minutesRequiredPerObject;
+            WeekNumbers = weekNumbers;
+            AvailableActors = availableActors;
+            AvailableProcesses = availableProcesses;
+        }
+
+        public bool IsValid()
+        {
+            if (AvailableActors == null ||
+                AvailableProcesses == null ||
+                NoOfObjects == 0 ||
+                MinutesRequiredPerObject == 0 ||
+                WeekNumbers == null)
+            {
+                return false;
+            }
+
+            return AvailableActors.Any() &&
+                   AvailableProcesses.Any() &&
+                   WeekNumbers.Any();
+        }
+
+        public void AddRow(TkResourceRequestSolutionRow row)
+        {
+            row.Group = this;
+            Rows.Add(row);
+            row.no = Rows.Count();
+        }
+        
+        public IEnumerable<TkSolution> Solve(int rowIndex = 0)
+        {
+            var ticksPerObject = TimeSpan.FromMinutes(MinutesRequiredPerObject).Ticks;
+            var totalTicksRequired = ticksPerObject * NoOfObjects;
+
+            var row = Rows[rowIndex];
+
+            // base case
+            if (rowIndex == Rows.Count - 1)
+            {
+                foreach (var response in row.Responses)
+                {
+                    yield return new TkSolution(response.Vacancy, new[] { response });
+                }
+            }
+
+            // recursive case
+            else
+            {
+                foreach (var currentSolution in Solve(rowIndex + 1))
+                {
+                    var vacancyFromPreviousRow = currentSolution.MutualVacancy; 
+                    var responsesFromPreviousRow = currentSolution.Responses;
+
+                    foreach (var response in row.Responses)
+                    {
+                        // 1. Check that there's sufficient overlap in vacancies.
+                        var vacancy = TimeSet.Intersect(vacancyFromPreviousRow, response.Vacancy);
+                        if (vacancy.GetOrderedIntervals().Where(o => o.Length().Ticks >= ticksPerObject)
+                                .Sum(o => o.Length().Ticks) < totalTicksRequired)
+                            continue;
+
+                        // 2. Check that we don't reuse the same actor twice.
+                        if (responsesFromPreviousRow.Any(o => o.Actor == response.Actor))
+                            continue;
+
+                        var updatedResponses = new TkResourceResponse[responsesFromPreviousRow.Length + 1];
+                        Array.Copy(responsesFromPreviousRow,
+                            0,
+                            updatedResponses,
+                            1,
+                            responsesFromPreviousRow.Length);
+
+                        updatedResponses[0] = response;
+                        yield return new TkSolution(vacancy, updatedResponses);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ TimeKit/Scheduling/TkResourceRequestSolutionRow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using TimeKit.DataStructure;
+using System.Linq;
+using TimeKit.Models;
+
+namespace TimeKit.Scheduling
+{
+    public class TkResourceRequestSolutionRow
+    {
+        public int no { get; set; }
+
+        public TkResourceRequestSolutionGroup Group { get; set; }
+
+        public TkResourceRequest Request { get; set; }
+        public List<TkResourceResponse> Responses { get; set; }
+
+        public TkResourceRequestSolutionRow(TkResourceRequest request, List<TkResourceResponse> responses)
+        {
+            Request = request;
+            Responses = responses;
+        }
+
+        public static TkResourceRequestSolutionRow NewRow(TkResourceRequestSolutionGroup group, TkResourceRequest request)
+        {
+            if (!request.IsValid())
+                return null;
+
+            var actors = group.AvailableActors.Where(o => o.Capabilities.Contains(request.RequiredCapability));
+            var responses = new List<TkResourceResponse>();
+            foreach (var actor in actors)
+            {
+                var processes = group.AvailableProcesses.Where(p => p.ParticipantId == actor.Key);
+                if (!processes.Any())
+                    continue;
+
+                var requestRunner = new TKResourceRequestRunner(group, request, actor, processes);
+                var response = requestRunner.Run();
+
+                if (response == null)
+                    continue;
+                
+                responses.Add(response);
+            }
+
+            var row = new TkResourceRequestSolutionRow(request, responses);
+            return row;
+        }
+    }
+}
