c2a2cc5fdca83b681b852bccff2430fa1cfe2dbe
Alexander Selling <alexander@icontrolapp.se>
2019-04-24 13:50:33 +0200

    Added a minimal scheduling class that simply subtracts e.g. 45 minutes from each time slot, each subtraction represents a 'scheduled' interval

---
+++ TimeKit/Scheduling/TkScheduler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TimeKit.DataStructure;
+
+namespace TimeKit.Scheduling
+{
+    public class TkScheduler
+    {
+        public static TimeSet Schedule(TkResourceRequestSolutionGroup group, TimeSet vacancy)
+        {
+            var scheduledIntervals = new List<Interval>();
+            var workSet = vacancy.Copy();
+
+            for (var objectNo = 0; objectNo < group.NoOfObjects; objectNo++)
+            {
+                var scheduledInterval = workSet.ExtractInterval(group.TicksRequiredPerObject);
+                if (scheduledInterval.isNull)
+                    return TimeSet.Null();
+
+                scheduledIntervals.Add(scheduledInterval);
+            }
+
+            var schedule = new TimeSet(scheduledIntervals.ToArray());
+            return schedule;
+        }
+    }
+}
