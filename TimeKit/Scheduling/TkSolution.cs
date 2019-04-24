1bac985be9ddcd7914b62b10d43a1caf4a6afad9
Alexander Selling <alexander@icontrolapp.se>
2019-04-24 13:51:00 +0200

    Each proposed solution by the Group.Solve algorithm gets stored in a TkSolution

---
+++ TimeKit/Scheduling/TkSolution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TimeKit.DataStructure;
+
+namespace TimeKit.Scheduling
+{
+    public class TkSolution
+    {
+        // When all of the actors in the responses array can work together
+        public TimeSet MutualVacancy { get; set; }
+        // Enumerates all of the actors and their respective vacancies
+        public TkResourceResponse[] Responses { get; set; }
+
+        public TkSolution(TimeSet mutualVacancy, TkResourceResponse[] responses)
+        {
+            MutualVacancy = mutualVacancy;
+            Responses = responses;
+        }
+
+        public override string ToString()
+        {
+            var actors = string.Join(", ", Responses.Select(o => $"{o.Actor.DisplayName}"));
+            return $"{actors} \n{MutualVacancy}";
+        }
+    }
+}
