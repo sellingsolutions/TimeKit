9c2716217bfdbca746a4c0946a4fe4f6e5a60e36
Alexander Selling <alexander@icontrolapp.se>
2019-07-10 15:48:57 +0200

    moved the TryScheduling method to a dedicated scheduling class

--- TimeKit/Scheduling/TkScheduler.cs
+++ TimeKit/Scheduling/TkScheduler.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using TimeKit.DataStructure;
+using TimeKit.Models;
 
 namespace TimeKit.Scheduling
 {
     public class TkScheduler
     {
-        public static TkTimeSet Schedule(TkSolution solution)
+        public static TkTimeSet TryScheduling(IEnumerable<TkTask> tasks, TkTimeSet vacancy)
         {
             var scheduledIntervals = new List<TkInterval>();
-            var workSet = solution.MutualVacancy.Copy();
+            var workSet = vacancy.Copy();
 
-            foreach (var task in solution.Request.Tasks)
+            foreach (var task in tasks)
             {
                 var scheduledInterval = workSet.ExtractInterval(TimeSpan.FromTicks(task.PlannedDuration));
                 if (scheduledInterval.isNull)
                     return TkTimeSet.Null();
 
+                task.ScheduledInterval = scheduledInterval;
                 scheduledIntervals.Add(scheduledInterval);
             }
 
