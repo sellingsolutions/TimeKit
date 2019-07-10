db09f104801b42cbbe84718088960305549f184b
Alexander Selling <alexander@icontrolapp.se>
2019-07-10 17:27:32 +0200

    In order for group 2 to account for the tentative schedule in group 1, we need to be able to add tentative placeholder processes that keep references their respective group solution

--- TimeKit/Models/TkProcess.cs
+++ TimeKit/Models/TkProcess.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TimeKit.Scheduling;
 
 namespace TimeKit.Models
 {
@@ -8,16 +9,29 @@ namespace TimeKit.Models
         public string Key { get; set; }
         public string ParticipantId { get; set; }
 
+        // Is this is process still just a placeholder?
+        public bool IsTentative { get; set; }
+        public TkSolution Solution { get; set; }
+
         public DateTime? StartsAt { get; set; }
         public DateTime? EndsAt { get; set; }
 
-        public TkProcess(ulong id, string key, string participantId, DateTime? startsAt, DateTime? endsAt)
+        public TkProcess(
+            ulong id,
+            string key, 
+            string participantId, 
+            DateTime? startsAt, 
+            DateTime? endsAt, 
+            bool isTentative = false,
+            TkSolution solution = null)
         {
             Id = id;
             Key = key;
             ParticipantId = participantId;
             StartsAt = startsAt;
             EndsAt = endsAt;
+            IsTentative = isTentative;
+            Solution = solution;
         }
     }
 }
